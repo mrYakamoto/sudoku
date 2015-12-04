473ff6d524a6109911bf809bab9069dbaa237160
Nil Thacker <nil@nil.gs>
2015-12-04 15:57:18 -0800

    Finish pretty print function

--- sudoku.rb
+++ sudoku.rb
@@ -76,11 +76,32 @@ class Sudoku
 
 
   def numbers_in_parallel_row(cell_x_index, cell_y_index)
-    # nil
+    # input: x index of cell (int) and y index of cell (int)
+    # output: 2d array consisiting of numbers from both parallel rows (array)
+    case cell_x_index % 3
+      when 0
+        puts "row is top"
+      when 1
+        puts "row is middle"
+        # if own_row is in the middle of the cube, return the rows to its top and bottom
+        left_row = @board[cell_x_index - 1]
+      when 2
+        puts "row is bottom"
+      end
   end
 
   def numbers_in_parallel_column(cell_x_index, cell_y_index)
-    # nil
+    # input: x index of cell (int) and y index of cell (int)
+    # output: 2d array consisiting of numbers from both parallel columns (array)
+
+    case cell_y_index % 3
+      when 0
+        puts "column is left"
+      when 1
+        puts "column is middle"
+      when 2
+        puts "column is right"
+      end
   end
 
   def number_empty_cells_in_own_cube_row(cell_x_index, cell_y_index)
@@ -90,15 +111,37 @@ class Sudoku
   end
 
   def solve
+    p "parallel rows: "
+    p numbers_in_parallel_row(1, 1)
+    p "parallel columns: "
+    p numbers_in_parallel_column(1, 1)
   end
 
-  def print_board
+  def pretty_print_board
+    puts "- - - - - - - - - - - - - - - -"
+    cube_horiz_print_counter = 1
     @board.each {|row|
-      p row
+      cube_vert_print_counter = 1
+      print "| "
+      row.each { |cell|
+        print " " + cell + " "
+        print "|" if cube_vert_print_counter == 3 || cube_vert_print_counter == 6
+        cube_vert_print_counter += 1
+      }
+      print "| "
+      puts
+      puts "- - - - - - - - - - - - - - - -" if cube_horiz_print_counter == 3 || cube_horiz_print_counter == 6
+      cube_horiz_print_counter += 1
     }
+    puts "- - - - - - - - - - - - - - - -"
   end
 
   # Returns a string representing the current state of the board
   def to_s
+    board_as_string = ""
+    board.each do |row|
+      board_as_string += row.join("")
+    end
+    return board_as_string
   end
 end
